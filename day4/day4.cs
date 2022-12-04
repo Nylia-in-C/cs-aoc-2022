1ae6934e0808a6618def71f7e26fdaf03c368832
Nylia-in-C <114269785+Nylia-in-C@users.noreply.github.com>
2022-12-04 13:14:03 -0700

    Solution to Day 4 Part 2

--- day4/day4.cs
+++ day4/day4.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Linq;
 namespace cleanup{
 
     class overlap
@@ -7,7 +6,7 @@ namespace cleanup{
         static void Main(string[] args){
             string[] input = File.ReadAllLines("input.txt");
             part1(input);
-            //part2(input);
+            part2(input);
         }
 
         static void part1(string[] input){
@@ -27,11 +26,27 @@ namespace cleanup{
                     count+=1;
                 }
             }
-            Console.WriteLine(count);
+            Console.WriteLine("Solution to part 1: {0:d}", count);
         }
         
          static void part2(string[] input){
-            
+            int count = 0;
+            foreach (string areas in input){ //read lines from file
+
+                //splits and conversions
+                string[] each_elf = areas.Split(',');
+                string[] elf1 = each_elf[0].Split('-'); string[] elf2 = each_elf[1].Split('-');
+                int elf1_i = Int32.Parse(elf1[0]); int elf1_f = Int32.Parse(elf1[1]);
+                int elf2_i = Int32.Parse(elf2[0]); int elf2_f = Int32.Parse(elf2[1]);
+
+                //check if one range overlaps the other
+                if (((elf1_i <= elf2_i) && (elf1_f >= elf2_i))  ||
+                    ((elf2_i <= elf1_i) && (elf2_f >= elf1_i))  ) {
+                    
+                    count+=1;
+                }
+            }
+            Console.WriteLine("Solution to part 2: {0:d}", count);
         }
     }
 }
