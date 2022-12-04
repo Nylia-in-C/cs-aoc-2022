c5bfa5a6bd00517c23a9c93643c2269c9707f684
Nylia-in-C <114269785+Nylia-in-C@users.noreply.github.com>
2022-12-04 13:07:14 -0700

    Day 4 Part 1 Solution

---
+++ day4/day4.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Linq;
+namespace cleanup{
+
+    class overlap
+    {
+        static void Main(string[] args){
+            string[] input = File.ReadAllLines("input.txt");
+            part1(input);
+            //part2(input);
+        }
+
+        static void part1(string[] input){
+            int count = 0;
+            foreach (string areas in input){ //read lines from file
+
+                //splits and conversions
+                string[] each_elf = areas.Split(',');
+                string[] elf1 = each_elf[0].Split('-'); string[] elf2 = each_elf[1].Split('-');
+                int elf1_i = Int32.Parse(elf1[0]); int elf1_f = Int32.Parse(elf1[1]);
+                int elf2_i = Int32.Parse(elf2[0]); int elf2_f = Int32.Parse(elf2[1]);
+
+                //check if one range is encapsulated by the other
+                if (((elf1_i <= elf2_i) && (elf1_f >= elf2_f))  ||
+                    ((elf1_i >= elf2_i) && (elf1_f <= elf2_f))  ) {
+                    
+                    count+=1;
+                }
+            }
+            Console.WriteLine(count);
+        }
+        
+         static void part2(string[] input){
+            
+        }
+    }
+}
